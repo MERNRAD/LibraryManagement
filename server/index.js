78f7ef6b8ab8c8560fa823ab3e4d85d8905b16d8
Kaveesha Muthukuda <2020cs118@stu.ucsc.cmb.ac.lk>
2022-10-24 11:53:10 +0530

    Database Connect

--- server/index.js
+++ server/index.js
@@ -1,8 +1,41 @@
 //express app
 
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 const app = express();
-const port = 3000;
+
+//import routes
+const postRoutes = require('./routes/posts');
+
+//Middleware App
+app.use(bodyParser.json());
+app.use(cors());
+
+//Middleware Routes
+app.use(postRoutes);
+
+
+
+
+const port = 8000;
+const DB_URL = 'mongodb+srv://kaveesha:kaveesha123@cluster0.hkneily.mongodb.net/LibraryManagement?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch((err) => console.log('DB connection error', err));
+
+
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
