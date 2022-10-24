2144df2c84b74406dc746db4409c0d79a3613d7f
AbdullahJasmin <abdullahmohjasmin@gmail.com>
2022-10-24 14:55:43 +0530

    Add borrowal schema, routes and controller

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 8080
 
 // Import routers
 const bookRouter = require("./routes/bookRouter")
+const borrowalRouter = require("./routes/borrowalRouter")
 
 // Connect to DB
 const mongoose = require('mongoose');
@@ -37,6 +38,7 @@ app.use(express.json())
 
 // Implement routes for REST API
 app.use("/api/book", bookRouter);
+app.use("/api/borrowal", borrowalRouter);
 
 app.get('/', (req, res) => res.send('Welcome to Library Management System'));
 
---
+++ server/routes/borrowalRouter.js
@@ -0,0 +1,24 @@
+// Import required modules
+const express = require("express")
+const router = express.Router();
+
+// Import functions from controller
+const {
+    getBorrowal,
+    getAllBorrowals,
+    addBorrowal,
+    updateBorrowal,
+    deleteBorrowal
+} = require('../controllers/BorrowalController')
+
+router.get("/getAll", (req, res) => getAllBorrowals(req,res))
+
+router.get("/get/:id", (req, res) => getBorrowal(req, res))
+
+router.post("/add", (req, res) => addBorrowal(req, res))
+
+router.put("/update/:id", (req, res) => updateBorrowal(req, res))
+
+router.put("/delete/:id", (req, res) => deleteBorrowal(req, res))
+
+module.exports = router;
