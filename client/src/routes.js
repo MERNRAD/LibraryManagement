9f0fb30736fe89bd05d129ea345901c93a0dac12
AbdullahJasmin <abdullahmohjasmin@gmail.com>
2022-10-25 23:24:42 +0530

    Update routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,19 +1,14 @@
 import {Navigate, useRoutes} from 'react-router-dom';
 // layouts
 import LibraryApp from './layouts/dashboard';
-//
-import BlogPage from './pages/BlogPage';
+// 
 import AuthorPage from './pages/AuthorPage';
 import LoginPage from './pages/LoginPage';
-import Page404 from './pages/Page404';
-
-import GenrePage from './pages/GenrePage';
-import BorrowalsPage from './pages/BorrowalsPage';
-
-import BooksPage from './pages/BooksPage';
-import UserPage from './pages/UserPage';
-import DashboardAppPage from './pages/DashboardAppPage';
-import Page404 from "./pages/Page404";
+import Page404 from './pages/Page404'; 
+import BorrowalsPage from './pages/BorrowalsPage'; 
+import BooksPage from './pages/BooksPage'; 
+import DashboardAppPage from './pages/DashboardAppPage'; 
+import UsersPage from './pages/UserPage'; 
 
 // ----------------------------------------------------------------------
 
@@ -27,10 +22,10 @@ export default function Router() {
         {element: <Navigate to="/app"/>, index: true},
         {path: 'dashboard', element: <DashboardAppPage/>},
         {path: 'authors', element: <AuthorPage/>},
-        {path: 'books', element: <ProductsPage/>},
-        {path: 'borrowals', element: <ProductsPage/>},
+        {path: 'books', element: <BooksPage/>},
+        {path: 'borrowals', element: <BorrowalsPage/>},
         {path: 'genres', element: <AuthorPage/>},
-        {path: 'users', element: <BlogPage/>},
+        {path: 'users', element: <UsersPage/>},
       ],
     },
     {
