8f659e62a17427602f7b3e8bf214d80cc14fb340
Sandul Renuja <sandulrenuja@gmail.com>
2022-10-25 14:17:44 +0530

    Implement react components for Author

---
+++ client/src/sections/@dashboard/author/AuthorDialog.jsx
@@ -0,0 +1,37 @@
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
+import PropTypes from "prop-types";
+
+AuthorDialog.propTypes = {
+  isDialogOpen: PropTypes.bool,
+  handleCloseDialog: PropTypes.func,
+  authorId: PropTypes.string,
+  handleDeleteAuthor: PropTypes.func
+};
+
+const AuthorDialog = ({isDialogOpen, handleCloseDialog, authorId, handleDeleteAuthor}) => {
+  return (
+    <Dialog
+      open={isDialogOpen}
+      onClose={handleCloseDialog}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        Confirm action
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          Are you sure you want to delete this author?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCloseDialog}>No</Button>
+        <Button onClick={() => handleDeleteAuthor(authorId)} autoFocus>
+          Yes
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default AuthorDialog
